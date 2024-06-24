b317f9014d88ad58ab868160ded18c8845bcd1da
devkartic <kartic.gharami@gmail>
2024-06-24 23:35:05 +0600

    Working with pagination customization background

--- resources/js/Pages/Admin/Partials/Pagination.vue
+++ resources/js/Pages/Admin/Partials/Pagination.vue
@@ -4,7 +4,7 @@
 })
 </script>
 <template>
-    <div>
-        <Component :is="link.url ? 'Link' : 'span'" v-for="link in links" :href="link.url" v-html="link.label" class="px-1" :class="{ 'text-gray-500': ! link.url, 'font-bold' : link.active }"/>
+    <div class="flex gap-1">
+        <Component :is="link.url ? 'Link' : 'span'" v-for="link in links" :href="link.url" v-html="link.label" class="text-gray-700 py-2 px-4 border border-cyan-500 rounded" :class="{ 'bg-transparent': ! link.url, 'bg-cyan-500 text-white border-cyan-500 font-bold' : link.active }"/>
     </div>
 </template>
